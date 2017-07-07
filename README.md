0bd33c41a71033a3b4fd0fc61c2d9232beb453ac
feiniaoyouyu <963128560@qq.com>
2017-07-07 10:25:19 +0800

    第一次修改

--- README.md
+++
@@ -1,1 +0,0 @@
-# feiniaoyouyu.github.io
--- aston/js/size.js
+++
@@ -1,9 +0,0 @@
-// 计算屏幕宽度 /20 设置给 html的font-size
-var fontSize = window.screen.width / 20;
-document.querySelector('html').style.fontSize = fontSize + 'px';
-// 屏幕尺寸发生改变时 重新计算html的font-size
-window.onresize = function () {
-    // 计算屏幕宽度 /20 设置给 html的font-size
-    var fontSize = window.screen.width / 20;
-    document.querySelector('html').style.fontSize = fontSize + 'px';
-}
