91d9fe739b6978b993fa7b1bce2afcec3dbe353f
feiniaoyouyu <963128560@qq.com>
2017-07-19 20:31:25 +0800

    第一次工作

---
+++ work/js/index.js
@@ -0,0 +1,34 @@
+$(function () {
+    // banner轮播图区
+    var topSwiper = new Swiper('.top-swiper-container', {
+        pagination: '#top-pagination-bar',
+        paginationClickable: true,
+        loop: true,
+        autoplay: 1500,
+        autoplayDisableOnInteraction: false,
+    });
+    /*神韵演出轮播图*/
+    var activeSwiper = new Swiper('.active-swiper-container', {
+        pagination: '#active-pagination-bar',
+        paginationClickable: true,
+        loop: true,
+        autoplay: 1500,
+        autoplayDisableOnInteraction: false,
+    });
+    /*天然温泉轮播图*/
+     var springSwiper = new Swiper('.spring-swiper-container', {
+        pagination: '#spring-pagination-bar',
+        paginationClickable: true,
+        loop: true,
+        autoplay: 1500,
+        autoplayDisableOnInteraction: false,
+    });
+    /*美食轮播图*/
+     var foodSwiper = new Swiper('.food-swiper-container', {
+        pagination: '#food-pagination-bar',
+        paginationClickable: true,
+        loop: true,
+        autoplay: 1500,
+        autoplayDisableOnInteraction: false,
+    });
+})
